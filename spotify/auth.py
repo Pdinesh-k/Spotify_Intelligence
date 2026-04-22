1e8a7d846c71300c3d3fd2c1fd7db73739d56cd6
Pdinesh-k <cheenu2801@gmail.com>
2026-04-22 20:38:46 +0530

    fix: remove cached Spotify token, use MemoryCacheHandler to prevent token sharing between users

--- spotify/auth.py
+++ spotify/auth.py
@@ -10,25 +10,21 @@ SCOPES = " ".join([
 ])
 
 
-def get_oauth_url(state: str = "intelligence") -> str:
-    sp_oauth = SpotifyOAuth(
+def _make_oauth(**kwargs) -> SpotifyOAuth:
+    return SpotifyOAuth(
         client_id=SPOTIFY_CLIENT_ID,
         client_secret=SPOTIFY_CLIENT_SECRET,
         redirect_uri=SPOTIFY_REDIRECT_URI,
         scope=SCOPES,
-        state=state,
-        show_dialog=True,
+        cache_handler=spotipy.cache_handler.MemoryCacheHandler(),
         open_browser=False,
+        **kwargs,
     )
-    return sp_oauth.get_authorize_url()
+
+
+def get_oauth_url(state: str = "intelligence") -> str:
+    return _make_oauth(state=state, show_dialog=True).get_authorize_url()
 
 
 def exchange_code(code: str) -> dict:
-    sp_oauth = SpotifyOAuth(
-        client_id=SPOTIFY_CLIENT_ID,
-        client_secret=SPOTIFY_CLIENT_SECRET,
-        redirect_uri=SPOTIFY_REDIRECT_URI,
-        scope=SCOPES,
-        open_browser=False,
-    )
-    return sp_oauth.get_access_token(code, as_dict=True)
+    return _make_oauth().get_access_token(code, as_dict=True)
